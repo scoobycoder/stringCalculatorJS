cf42b32067ad7ea098f3b8d48a6b8185d0e5c681
Nate <nathan_halterman@gap.com>
2016-10-23 15:31:32 -0400

    more refactoring

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const add = (numbers) => ((numbers.split('\n').toString().split(',')))
+const add = (numbers) => (numbers.split('\n').toString().split(','))
     .reduce((total, num) => (parseInt(total) + parseInt(num)));
 
 module.exports = {
