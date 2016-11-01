66de8bd625013909f62333f36a24a68fb4440a42
Nate <nathan_halterman@gap.com>
2016-11-01 10:07:43 -0400

    moving function to one line

--- calculator.js
+++ calculator.js
@@ -3,9 +3,7 @@ const add = (numbers) => {
 
   const result = numbers.split('\n')
     .map((numbers) => numbers.split(','))
-    .reduce(
-        (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
-    )
+    .reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), [])
     .reduce((total, num) => parseInt(total) + parseInt(num))
   return result
 }
