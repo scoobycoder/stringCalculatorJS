2b65852657aa5b584427554bbb33c76b14ba6d6c
Nate <nathan_halterman@gap.com>
2016-11-20 15:04:07 -0500

    in the middle of promises

--- calculator.js
+++ calculator.js
@@ -5,7 +5,43 @@ const add = (numbers) => {
   return removeCharacters(numbers).split(getDelimiter(numbers))
     .filter(Boolean)
     .reduce((total, num) => parseInt(total) + parseInt(num))
+
+
+    Promise.all([removeCharactersPromise, getArrayPromise, arrayFilterPromise, sumNumbersPromise])
+      .then((res) => {
+        return(res)
+      }
+
+
 }
+
+
+
+const removeCharactersPromise = new Promise(function(resolve, reject) {
+  () => resolve((numbers) => numbers.replace('\n','').replace('//',''))
+})
+
+const getDelimiterPromise = new Promise(function(resolve, reject) {
+  () => resolve((numbers) => numbers.substr(numbers.indexOf('//') + 2, numbers.indexOf('//') + 1))
+})
+
+const getArrayPromise = new Promise(function(resolve, reject) {
+  () => resolve((numbers) => numbers.split(getDelimiterPromise(numbers)))
+})
+
+const arrayFilterPromise = new Promise(function(resolve, reject) {
+  () => resolve((numbers) => numbers.filter(Boolean))
+})
+
+const sumNumbersPromise = new Promise(function(resolve, reject) {
+  () => resolve((numbers) => numbers.reduce((total, num) => parseInt(total) + parseInt(num)))
+})
+
+p.then((res) => {
+  res += 2;
+  console.log(res);
+});
+
 
 module.exports = {
     add
